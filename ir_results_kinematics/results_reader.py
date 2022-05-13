d73e8272a8a8fdcbb9aa95b1c08898b3e20125e2
Bevan Koopman <bevan@koopman.id.au>
2022-05-13 14:27:14 +1000

    Error handling and Python 3 support

--- ir_results_kinematics/results_reader.py
+++ ir_results_kinematics/results_reader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Reads results file, return dict of query_id -> (score, results_line, doc, pos)
+# Reads results file, return dict of query_id -> (score, results_line, doc, pos, runid)
 # 
 
 
@@ -14,15 +14,17 @@ def read(fh):
         score = float(items[4])
         doc = items[2]
         pos = items[3]
+        runid = items[5]
     
-        results[query] = results[query] + [(score, line.strip(), doc, pos)] if query in results else [(score, line.strip(), doc, pos)]
+        results[query] = results[query] + [(score, line.strip(), doc, pos, runid)] if query in results else [(score, line.strip(), doc, pos, runid)]
 
     return results
 
 if __name__ == "__main__":
 	args = argparse.ArgumentParser(description="Reads a set of TREC results.")
-	args.add_argument("results_file")
 	args.add_argument("qrel_file", nargs='?')
+	args.add_argument("results_file")
+	
 	qrels = {}
 	if args.parse_args().qrel_file != None:
 		import qrel_reader
@@ -30,8 +32,6 @@ if __name__ == "__main__":
 
 	results = read(open(args.parse_args().results_file))
 	for (qId, docs) in sorted(results.items()):
-		for (score, line, doc, pos) in docs:
-			print line,
-			if qId in qrels and doc in qrels[qId]:
-				print qrels[qId][doc],
-			print ''
+		for (score, line, doc, pos, runid) in docs:
+			relevance = qrels[qId][doc] if qId in qrels and doc in qrels[qId] else ''
+			print("{}\t{}".format(line, relevance))
